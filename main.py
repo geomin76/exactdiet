903d1ec4f6a20550fea325d07569754a21bec23a
geomin76 <geom@vt.edu>
2020-09-14 20:56:28 -0400

    Grabbing line by line for images

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 import requests
 from flask import Flask
+import json
 
 app = Flask(__name__)
 
@@ -17,26 +18,22 @@ def test():
     ocr_url = endpoint + "vision/v3.0/ocr"
 
     # Set image_url to the URL of an image that you want to analyze.
-    image_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/" + \
-        "Atomist_quote_from_Democritus.png/338px-Atomist_quote_from_Democritus.png"
+    image_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Atomist_quote_from_Democritus.png/338px-Atomist_quote_from_Democritus.png"
 
     headers = {'Ocp-Apim-Subscription-Key': subscription_key}
-    params = {'language': 'unk', 'detectOrientation': 'true'}
+    params = {'language': 'en', 'detectOrientation': 'true'}
     data = {'url': image_url}
     response = requests.post(ocr_url, headers=headers, params=params, json=data)
     response.raise_for_status()
 
     analysis = response.json()
 
-    # Extract the word bounding boxes and text.
-    line_infos = [region["lines"] for region in analysis["regions"]]
-    word_infos = []
-    for line in line_infos:
-        print(line)
-    #     for word_metadata in line:
-    #         for word_info in word_metadata["words"]:
-    #             word_infos.append(word_info)
-    # print(word_infos)
+    res = []
+    for region in analysis["regions"]:
+        for lines in region["lines"]:
+            s = " ".join(words["text"] for words in lines["words"])
+            res.append(s)
+    print(res)
     return "woo"
 
 if __name__ == '__main__':
