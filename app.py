02d531751536b7e61e8bf1c312809e1cd19c194f
geomin76 <geom@vt.edu>
2020-09-19 21:42:38 -0400

    Adding microsoft OCR

--- app.py
+++ app.py
@@ -1,39 +1,39 @@
-# import os
-# import sys
-# import requests
+import os
+import sys
+import requests
 from flask import Flask
-# import json
+import json
 
 app = Flask(__name__)
 
-# subscription_key = os.environ['COMPUTER_VISION_SUBSCRIPTION_KEY']
-# endpoint = os.environ['COMPUTER_VISION_ENDPOINT']
+subscription_key = os.environ['COMPUTER_VISION_SUBSCRIPTION_KEY']
+endpoint = os.environ['COMPUTER_VISION_ENDPOINT']
 
 @app.route('/')
 def hello():
     return "Hello World!"
 
-# @app.route('/analyze')
-# def test():
-#     ocr_url = endpoint + "vision/v3.0/ocr"
-#     image_url = "./Capture.PNG"
+@app.route('/analyze')
+def test():
+    ocr_url = endpoint + "vision/v3.0/ocr"
+    image_url = "./Capture.PNG"
 
-#     image_data = open(image_url, "rb").read()
+    image_data = open(image_url, "rb").read()
 
-#     headers = {'Content-Type': 'application/octet-stream', 'Ocp-Apim-Subscription-Key': subscription_key}
-#     params = {'language': 'en', 'detectOrientation': 'true'}
-#     response = requests.post(ocr_url, headers=headers, params=params, data=image_data)
-#     response.raise_for_status()
+    headers = {'Content-Type': 'application/octet-stream', 'Ocp-Apim-Subscription-Key': subscription_key}
+    params = {'language': 'en', 'detectOrientation': 'true'}
+    response = requests.post(ocr_url, headers=headers, params=params, data=image_data)
+    response.raise_for_status()
 
-#     analysis = response.json()
+    analysis = response.json()
 
-#     res = []
-#     for region in analysis["regions"]:
-#         for lines in region["lines"]:
-#             s = " ".join(words["text"] for words in lines["words"])
-#             res.append(s)
-#     print(res)
-#     return "woo"
+    res = []
+    for region in analysis["regions"]:
+        for lines in region["lines"]:
+            s = " ".join(words["text"] for words in lines["words"])
+            res.append(s)
+    print(res)
+    return "woo"
 
 # if __name__ == '__main__':
 #     app.run()
