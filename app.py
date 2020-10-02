7952d4c70d31558c7ea4d4333d16e1b683b12c3b
geomin76 <geom@vt.edu>
2020-10-01 22:50:47 -0400

    Added tesseract executable

--- app.py
+++ app.py
@@ -1,40 +1,20 @@
 import os
 import sys
-import requests
 from flask import Flask, request
 import json
 
 app = Flask(__name__)
-
-subscription_key = os.environ['COMPUTER_VISION_SUBSCRIPTION_KEY']
-endpoint = os.environ['COMPUTER_VISION_ENDPOINT']
 
 @app.route('/')
 def hello():
     return "Hello World!"
 
 @app.route('/analyze')
-def analyze():
-    image_url = str(request.args.get('url'))
-
-    ocr_url = endpoint + "vision/v3.0/ocr"
-
-    image_data = open(image_url, "rb").read()
-
-    headers = {'Content-Type': 'application/octet-stream', 'Ocp-Apim-Subscription-Key': subscription_key}
-    params = {'language': 'en', 'detectOrientation': 'true'}
-    response = requests.post(ocr_url, headers=headers, params=params, data=image_data)
-    response.raise_for_status()
-
-    analysis = response.json()
-
-    res = []
-    for region in analysis["regions"]:
-        for lines in region["lines"]:
-            s = " ".join(words["text"] for words in lines["words"])
-            res.append(s)
-    print(res)
-    return "woo"
+def test():
+    stream = os.popen('./tesseract ok.jpg stdout -l eng')
+    output = stream.read()
+    print(output)
+    return "ok"
 
 
 if __name__ == '__main__':
