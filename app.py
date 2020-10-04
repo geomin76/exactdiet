960221b69da4077758daca6aa2ac8865cb73e50d
geomin76 <geom@vt.edu>
2020-10-04 14:28:02 -0400

    Adding microsoft ocr

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from flask import Flask, request
+import requests
 import json
 import os
 import base64
@@ -8,8 +9,10 @@ from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 cors = CORS(app)
-# cors = CORS(app, resources={r"/analyze": {"origins": "https://gy0s1wfur7.execute-api.us-east-1.amazonaws.com/dev/"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
+
+subscription_key = os.environ['COMPUTER_VISION_SUBSCRIPTION_KEY']
+endpoint = os.environ['COMPUTER_VISION_ENDPOINT']
 
 @app.route('/')
 def hello():
@@ -22,16 +25,32 @@ def analyze():
     # print(request.json["data"])
 
     imgdata = base64.b64decode(encoded_string)
-    filename = '/tmp/img.jpg'
+    filename = '/tmp/img.png'
     with open(filename, 'wb') as f:
         f.write(imgdata)
     
-    stream = os.popen('./tesseract/tesseract /tmp/img.jpg stdout -l eng')
-    output = stream.read()
-    print(output)
-    os.remove('/tmp/img.jpg')
+    # stream = os.popen('./ocr/tesseract /tmp/img.jpg stdout -l eng')
+    # output = stream.read()
+    # print(output)
+
+    ocr_url = endpoint + "vision/v3.0/ocr"
+    image_url = '/tmp/img.png'
+    image_data = open(image_url, "rb").read()
+    headers = {'Content-Type': 'application/octet-stream', 'Ocp-Apim-Subscription-Key': subscription_key}
+    params = {'language': 'en', 'detectOrientation': 'true'}
+    response = requests.post(ocr_url, headers=headers, params=params, data=image_data)
+    response.raise_for_status()
+    analysis = response.json()
+    res = []
+    for region in analysis["regions"]:
+        for lines in region["lines"]:
+            s = " ".join(words["text"] for words in lines["words"])
+            res.append(s)
+    print(res)
+
+    os.remove('/tmp/img.png')
     
-    return json.dumps({'data': str(output)})
+    return json.dumps({ 'data': " ".join(res) })
 
 
 if __name__ == '__main__':
