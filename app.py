68531aeedf53c796d53d8bd8a340a52dcb343076
geomin76 <geom@vt.edu>
2020-10-04 13:29:26 -0400

    Adding

--- app.py
+++ app.py
@@ -30,16 +30,6 @@ def analyze():
     output = stream.read()
     print(output)
     os.remove('/tmp/img.jpg')
-
-    # return {
-    #     'statusCode': 200,
-    #     'headers': {
-    #         'Access-Control-Allow-Headers':  "*",
-    #         'Access-Control-Allow-Origin': '*',
-    #         'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-    #     },
-    #     'body': json.dumps({'data': str(output)})
-    # }
     
     return json.dumps({'data': str(output)})
 
